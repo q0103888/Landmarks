d56361ea1550c1bf5311f58c2130a3a62e3e32e9
Landmarks <q0103888@gmail.com>
2022-01-04 20:16:50 +0900

    Initial Commit

---
+++ Shared/LandmarksApp.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarksApp.swift
+//  Shared
+//
+//  Created by HyounHo Lee on 2022/01/04.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+            //test git
+        }
+    }
+}
